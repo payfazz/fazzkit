ada49aa14f330d0b792463bce9a234f9f409a28e
Michael Reynaldo Phangtriastu <mikaelphang@gmail.com>
2019-07-22 21:46:01 +0700

    test on http server

---
+++ server/server_test.go
@@ -0,0 +1,53 @@
+package server_test
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi"
+	"github.com/payfazz/fazzkit/server"
+)
+
+func Foo(ctx context.Context, request interface{}) (response interface{}, err error) {
+	return nil, nil
+}
+
+func TestFoo(t *testing.T) {
+	m := chi.NewRouter()
+	m.Get("/foo", server.NewHTTPServer(Foo, nil).ServeHTTP)
+
+	ts := httptest.NewServer(m)
+	defer ts.Close()
+
+	res, _ := testRequest(t, ts, "GET", "/foo", nil)
+	if res.StatusCode != 200 {
+		t.Fatal(res)
+	}
+}
+
+func testRequest(t *testing.T, ts *httptest.Server, method, path string, body io.Reader) (*http.Response, string) {
+	req, err := http.NewRequest(method, ts.URL+path, body)
+	if err != nil {
+		t.Fatal(err)
+		return nil, ""
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+		return nil, ""
+	}
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+		return nil, ""
+	}
+	defer resp.Body.Close()
+
+	return resp, string(respBody)
+}
