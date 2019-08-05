a1f3e59ead5f6e55041004603545fe999354c7c4
Michael Reynaldo Phangtriastu <mikaelphang@gmail.com>
2019-08-05 20:11:12 +0700

    flyweight logger

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -2,7 +2,9 @@ package middleware
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/log"
@@ -15,24 +17,38 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+var loggers = make(map[string]logger.Logger)
+var lock sync.Mutex
+
 //LogAndInstrumentation wrap endpoint function with logger.Log and logger.Instrumentation
 //This middleware will measure request_count and request_latency_microseconds
 func LogAndInstrumentation(kitLogger log.Logger, namespace string, subsystem string, action string) endpoint.Middleware {
-	logObj := logger.New(
-		kitprometheus.NewCounterFrom(prometheus.CounterOpts{
-			Namespace: namespace,
-			Subsystem: subsystem,
-			Name:      "request_count",
-			Help:      "Number of request received.",
-		}, []string{"method"}),
-		kitprometheus.NewSummaryFrom(prometheus.SummaryOpts{
-			Namespace: namespace,
-			Subsystem: subsystem,
-			Name:      "request_latency_microseconds",
-			Help:      "Total duration of requests in microseconds.",
-		}, []string{"method"}),
-		kitLogger,
-	)
+	var logObj logger.Logger
+
+	key := fmt.Sprintf("%s_%s", namespace, subsystem)
+	if val, ok := loggers[key]; ok {
+		logObj = val
+	} else {
+		lock.Lock()
+		logObj = logger.New(
+			kitprometheus.NewCounterFrom(prometheus.CounterOpts{
+				Namespace: namespace,
+				Subsystem: subsystem,
+				Name:      "request_count",
+				Help:      "Number of request received.",
+			}, []string{"method"}),
+			kitprometheus.NewSummaryFrom(prometheus.SummaryOpts{
+				Namespace: namespace,
+				Subsystem: subsystem,
+				Name:      "request_latency_microseconds",
+				Help:      "Total duration of requests in microseconds.",
+			}, []string{"method"}),
+			kitLogger,
+		)
+
+		loggers[key] = logObj
+		lock.Unlock()
+	}
 
 	return func(f endpoint.Endpoint) endpoint.Endpoint {
 		return logObj.Instrumentation("method", action, logObj.Log("method", action, f))
