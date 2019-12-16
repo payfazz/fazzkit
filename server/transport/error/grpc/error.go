8313be5ad2b79291a6f95cb982c039930c735431
Fadhil Nur Mahardi <fadhil.avenged55@gmail.com>
2019-12-16 18:30:13 +0700

    +error mapper #22

---
+++ server/transport/error/grpc/error.go
@@ -0,0 +1,37 @@
+package grpc
+
+import (
+	"google.golang.org/grpc/codes"
+)
+
+type TransportError struct {
+	Error error
+	Code  codes.Code
+}
+
+type ErrorMapper struct {
+	Error map[error]*TransportError
+}
+
+func NewErrorMapper() *ErrorMapper {
+	return &ErrorMapper{
+		Error: make(map[error]*TransportError),
+	}
+}
+
+func (e *ErrorMapper) RegisterError(err error, grpcCode codes.Code) {
+	newTransportError := TransportError{
+		Error: err,
+		Code:  grpcCode,
+	}
+
+	e.Error[err] = &newTransportError
+}
+
+func (e *ErrorMapper) GetCode(err error) codes.Code {
+	if e.Error[err] == nil {
+		return codes.Internal
+	}
+
+	return e.Error[err].Code
+}
