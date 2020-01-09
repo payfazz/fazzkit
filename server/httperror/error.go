482bc68b79309e0b72c21e6ecb34e96bc9a4348f
Fadhil Nur Mahardi <fadhil.avenged55@gmail.com>
2020-01-09 20:38:35 +0700

    +error encoder with internal code #26

--- server/httperror/error.go
+++ server/httperror/error.go
@@ -4,16 +4,18 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+
+	transportError "github.com/payfazz/fazzkit/server/transport/error"
 )
 
 //ErrorWithStatusCode error with http status code
 type ErrorWithStatusCode struct {
-	Err        string
+	Err        error
 	StatusCode int
 }
 
 func (e *ErrorWithStatusCode) Error() string {
-	return e.Err
+	return e.Err.Error()
 }
 
 //EncodeError ...
@@ -28,5 +30,25 @@ func EncodeError(_ context.Context, err error, w http.ResponseWriter) {
 	w.WriteHeader(code)
 	json.NewEncoder(w).Encode(map[string]interface{}{
 		"error": err.Error(),
+	})
+}
+
+//EncodeErrorWithInternalCode ...
+func EncodeErrorWithInternalCode(_ context.Context, err error, w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	code := http.StatusInternalServerError
+	internalCode := "-1"
+	if errWithStatusCode, ok := err.(*ErrorWithStatusCode); ok {
+		code = errWithStatusCode.StatusCode
+		if errWithInternalCode, ok := errWithStatusCode.Err.(*transportError.ErrorWithInternalCode); ok {
+			internalCode = errWithInternalCode.Code
+		}
+	}
+
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"error": err.Error(),
+		"code":  internalCode,
 	})
 }
