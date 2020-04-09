0b96c4dc467838e1ef3c388d52ad07811bedd746
Fadhil Nur Mahardi <fadhil.avenged55@gmail.com>
2020-04-09 18:54:21 +0700

    +parse url encoded #30

--- server/http/parse.go
+++ server/http/parse.go
@@ -4,12 +4,28 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/schema"
 )
 
 //ParseJSON parse request body (json) to model
 func ParseJSON(ctx context.Context, request *http.Request, model interface{}) (interface{}, error) {
 	err := json.NewDecoder(request.Body).Decode(model)
 
+	if err != nil {
+		return nil, err
+	}
+
+	return model, nil
+}
+
+func ParseURlEncoded(ctx context.Context, request *http.Request, model interface{}) (interface{}, error) {
+	err := request.ParseForm()
+	if err != nil {
+		return nil, err
+	}
+	decoder := schema.NewDecoder()
+	err = decoder.Decode(model, request.PostForm)
 	if err != nil {
 		return nil, err
 	}
